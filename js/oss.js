818bafc2156f47f8dfca4b32f398b06c866a1460
Tim Bielawa <tbielawa@redhat.com>
2013-12-05 22:40:34 -0500

    Tweaks. Like style.

--- js/oss.js
+++ js/oss.js
@@ -17,4 +17,51 @@ $(document).ready(function(){
         'html': true,
         'content': "<a href='http://oit.wvu.edu/ctec/files/2009/01/floorplan-esbg102.jpg' target='_blank'>Location</a><br /><a href='http://www.statler.wvu.edu/direction.php' target='_blank'>Driving Directions</a>",
     });
+
+    jQuery(function($){
+        // unordered list
+        $('button.item').click(function(){
+            $('ul').shuffle();
+        });
+
+        // a series of paragraphs
+        $('button.div').click(function(){
+            $('#container').shuffle();
+        });
+
+        // an array
+        var arr = [1,2,3,4,5,6];
+        $('#array').text(arr.join(', '));
+        $('button.array').click(function(){
+            arr = $.shuffle(arr);
+            $('#array').text(arr.join(', '));
+        });
+    });
+
+
+    (function($){
+        $.fn.shuffle = function() {
+            return this.each(function(){
+                var items = $(this).children().clone(true);
+                return (items.length) ? $(this).html($.shuffle(items)) : this;
+            });
+        }
+
+        $.shuffle = function(arr) {
+            for(var j, x, i = arr.length; i; j = parseInt(Math.random() * i), x = arr[--i], arr[i] = arr[j], arr[j] = x);
+            return arr;
+        }
+
+    })(jQuery);
+
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Congrats! You found the easter egg! Press the 'r' key to randomize.
+    ////////////////////////////////////////////////////////////////////////////
+    $('#boardmembers').shuffle();
+    $(document).keyup(function(event) {
+        if ( event.which == 82 ) {
+            $('#boardmembers').shuffle();
+        }
+    });
 });
